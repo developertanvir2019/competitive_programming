48c7bdafd52f085eba4aff3ee6e58b367ba1f1a9
developertanvir2019 <developertanvir2019@gmail.com>
2025-05-06 22:48:16 +0600

    complete stock prolemm

--- leetcode/rotate_array.cpp
+++ leetcode/rotate_array.cpp
@@ -15,3 +15,26 @@ class Solution {
 			nums = newArr;
 		}
 	};
+
+
+	// mx profit
+
+	/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPz=prices[0];
+    let mxProfit=0;
+    for(let i=1;i<prices.length;i++){
+        const profit=prices[i]-minPz;
+        if(profit>mxProfit){
+            mxProfit=profit;
+        }
+        if(minPz>prices[i]){
+            minPz=prices[i];
+        }
+        
+    }
+    return mxProfit;
+};
